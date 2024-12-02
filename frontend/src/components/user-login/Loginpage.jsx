d686fcbb69001f99fcc2b62fd61db355fb18033b
Yashaswini-ks <yashu1993@gmail.com>
2024-12-02 17:27:14 +0530

    Adding Email error validation

--- frontend/src/components/user-login/Loginpage.jsx
+++ frontend/src/components/user-login/Loginpage.jsx
@@ -3,6 +3,7 @@ import api from "../../api";
 import { useNavigate } from "react-router-dom";
 import { TOKEN } from "../../constants";
 import "../../styles/Form.css";
+import { validate as validateEmail } from 'email-validator';
 
 
 
@@ -10,14 +11,20 @@ function LoginPage({ route, method }) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const [hasError,setHasError] = useState(false)
     const navigate = useNavigate();
 
     const name = method === "login" ? "Login" : "Register";
 
+    const validateOnSave = () => {
+       return validateEmail(username)
+    }
+
     const handleSubmit = async (e) => {
         setLoading(true);
         e.preventDefault();
-
+        const valid  = validateOnSave();
+        if(valid){
         try {
             const res = await api.post(route, { username, password })
             if (method === "login") {
@@ -30,10 +37,11 @@ function LoginPage({ route, method }) {
             alert(error)
         } finally {
             setLoading(false)
+        }}
+        else{
+            setHasError(true)
         }
     };
-
-   
 
     return (
         <div class="container">
@@ -41,7 +49,7 @@ function LoginPage({ route, method }) {
             <div class="logo">
                 <img src="https://www.bayer.com/themes/custom/bayer_cpa/logo.svg" width="80" alt="Bayer Healthcare Logo"/>
             </div>
-            <h1>Login</h1>
+            <h1>{method =="login"?"Login": "Register"}</h1>
             <form class="loginform">
                 <input type="email" placeholder="Email" required="" value={username}
                 onChange={(e) => setUsername(e.target.value)}/>
