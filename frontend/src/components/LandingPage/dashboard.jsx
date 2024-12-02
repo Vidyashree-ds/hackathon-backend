9efb3f368a9a1f6daa33a26f0db6234d2b0bb0b2
beekaye1234 <beekaye1234@gmail.com>
2024-12-02 15:47:32 +0530

    Dashboard page initial commit

---
+++ frontend/src/components/LandingPage/dashboard.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import Header from "../layout/header";
+import Footer from "../layout/footer";
+
+const dashboard = () => {
+  return (
+    <>
+      <Header />
+      <div className="main-container">Dashboard Page</div>
+      <Footer />
+    </>
+  );
+};
+
+export default dashboard;
---
+++ frontend/src/components/layout/footer.jsx
@@ -0,0 +1,12 @@
+import React,{ useState } from "react";
+
+const footer = () =>{
+return (
+    <footer>
+    <h1>Bayer Healthcare</h1>
+</footer>
+)
+
+}
+
+export default footer
